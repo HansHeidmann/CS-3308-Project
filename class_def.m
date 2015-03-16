adcc9ff9b9b8020718ed7d238db05a20a383bbb8
Sean <sean.tranchetti@colorado.edu>
2015-03-15 18:29:54 -0600

    added class definition file

---
+++ class_def.m
@@ -0,0 +1,41 @@
+//Basic Class info that we came up with!
+
+/*class BluetoothSend
+  -should send info to the arduino wristband
+  -pulls info from the UserPref class
+  -current route to run against
+  -delete current GPS data on Arduino
+*/
+
+/*class BluetoothRecieve
+  -recieve data from Arduino
+  -create routes as necessary
+*/
+
+/*class Graph
+  + associated Route
+*/
+
+/*class UserPref
+  +favorite route
+  +current route(or new)
+  +current time(or new)
+*/
+
+/*class OverallSettings
+  +route sorting setting (date/time)
+  +units of measure (miles, km)
+*/
+
+/*class Route
+  +ID
+  +database query functionality
+  +overall time
+  +overall distance
+*/
+
+/*Database
+  -GPS coord
+  -Times
+  -Users (IDs)
+*/
