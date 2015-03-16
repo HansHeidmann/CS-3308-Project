a8152be0635ea66054582e3f85f186f13dc79f2c
Sean <sean.tranchetti@colorado.edu>
2015-03-15 20:25:50 -0600

    updated with ObjC syntax

--- class_def.m
+++ class_def.m
@@ -6,26 +6,48 @@
   -current route to run against
   -delete current GPS data on Arduino
 */
+@interface BluetoothSend
+//stuff to read info from UserPref
+@end
 
 /*class BluetoothRecieve
   -recieve data from Arduino
   -create routes as necessary
 */
+@interface BluetoothRec
+//stuff to create routes
+@end
 
 /*class Graph
   + associated Route
 */
+@interface Graph {
+  Route route;
+}
+//graph draw functions
+@end
 
 /*class UserPref
   +favorite route
   +current route(or new)
   +current time(or new)
 */
+@interface UserPref {
+  Route favorite;
+  Route current;
+  double time;
+}
+@end
 
 /*class OverallSettings
   +route sorting setting (date/time)
   +units of measure (miles, km)
 */
+@interface Settings {
+  NSString sort;
+  NSString units;
+}
+@end
 
 /*class Route
   +ID
@@ -33,6 +55,13 @@
   +overall time
   +overall distance
 */
+@interface Route {
+  int ID;
+  double time;
+  float distance;
+}
+//database query functions
+@end
 
 /*Database
   -GPS coord
