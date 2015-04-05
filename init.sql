cde12ed0943e4bbd4ff2bd4e219e07ab49e72907
Sean <sean.tranchetti@colorado.edu>
2015-04-05 14:12:13 -0600

    added new columns into databse tables

--- init.sql
+++ init.sql
@@ -6,12 +6,21 @@ CREATE TABLE IF NOT EXISTS "Routes" (
 
 CREATE TABLE IF NOT EXISTS "Coordinates" (
 "ID" INTEGER PRIMARY KEY,
-"RouteID" int REFERENCES "Routes"("ID") ON DELETE CASCADE ON UPDATE CASCADE
+"RouteID" int REFERENCES "Routes"("ID") ON DELETE CASCADE ON UPDATE CASCADE,
+"Latitude" float,
+"Longitude" float,
+"Altitude" int
 )
 ;
 
 CREATE TABLE IF NOT EXISTS "Times" (
 "ID" INTEGER PRIMARY KEY,
-"CoordinateID" int REFERENCES "Coordinates"("ID") ON DELETE CASCADE ON UPDATE CASCADE
+"CoordinateID" int REFERENCES "Coordinates"("ID") ON DELETE CASCADE ON UPDATE CASCADE,
+"Year" int,
+"Month" int,
+"Day" int,
+"Hour" int,
+"Minute" int,
+"Second" int
 )
 ;
