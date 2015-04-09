1e693fe2c02929d1df105a90be1d95afc9b5e217
Sean <sean.tranchetti@colorado.edu>
2015-04-09 16:13:56 -0600

    added dummy uuid values

--- BTSendRec.h
+++ BTSendRec.h
@@ -7,16 +7,16 @@
 
 #define BLE_UUID [CBUUID UUIDWithString: @"27EC8B94-9C1A-FE12-3669-780CD087A7A4"]
 
-#define RX_UUID
+#define RX_UUID [CBUUUID UUIDWithString: @"00000000-0000-0000-0000-000000000000"]
 
-#define TX_UUID
+#define TX_UUID [CBUUID UUIDWIthString: @"00000000-0000-0000-0000-000000000000"]
 
 #define RWT_BLE_SERVICE_CHANGED_STATUS_NOTIFICATION @"stuff goes here"
 
 //wrapper around CBPeripheral and acts as its delegate object
 //responsible for doing the data transmission to the connected peripheral
 @interface BTSendRec : NSObject <CBPeripheralDelegate>
-- (instancetype) initWithPeripheral:(CBPeripheral* peripheral);
+- (instancetype) initWithPeripheral:(CBPeripheral*) peripheral;
 - (void) startDiscoveringServices;
 - (void) reset;
 - (void) peripheral: (CBPeripheral*) peripheral didDiscoverServices: (NSError*) error;
