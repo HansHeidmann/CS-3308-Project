0e857902ca93879709a95ad223c72006d6f3eac2
Sean <sean.tranchetti@colorado.edu>
2015-04-09 15:44:14 -0600

    added bridge casts and first attempts CPU macros

--- database.m
+++ database.m
@@ -6,6 +6,7 @@
 @synthesize results = _results;
 @synthesize columns = _columns;
 
+#ifndef __arm__
 //destructor
 - (void) dealloc {
   [_file release];
@@ -13,6 +14,7 @@
   [_columns release];
   [super dealloc];
 }
+#endif
 
 //constructor
 - (instancetype) init_dbfile: (NSString*) filename {
@@ -27,7 +29,7 @@
 //callback method on the database object supplied as a void*
 //Objective-C doesn't like callbacks, hence the workaround
 int call_dummy(void* db, int col, char** res, char** names) {
-  return [(id) db callback: col Results: res ColText: names];
+  return [(__bridge id) db callback: col Results: res ColText: names];
 }
 
 //callback function called on each row of the results of a select query
@@ -71,34 +73,10 @@ int call_dummy(void* db, int col, char** res, char** names) {
   //execute our query and call call_dummy() on each row of results
   //self (the database object) gets passed as the first argument to the
   //callback
-  CHECK(sqlite3_exec(db, [query UTF8String], &call_dummy, self, NULL), res);
+  CHECK(sqlite3_exec(db, [query UTF8String], &call_dummy, (__bridge void*) self, NULL), res);
   //close our database connection
   CHECK(sqlite3_close(db), res);
   //NSLog(@"closed databse");
   return 0; //we've made it this far without returning. All's good
 }
 @end
-
-/*
-int main () {
-  @autoreleasepool {
-    Database* test = [[Database alloc] init_dbfile: @"database.db"];
-    NSLog(@"insert res: %i", [test query: @"INSERT INTO \"Routes\" (\"ID\", \"Name\") VALUES (1, 'test');"]);
-    NSLog(@"select res: %i", [test query: @"SELECT * FROM \"Routes\";"]);
-    NSLog(@"test filename: %@", test.file);
-    int rows = [test.results count];
-    int col = [test.columns count];
-    NSLog(@"result row count: %i", rows);
-    NSLog(@"result column count: %i", col);
-    for (int i = 0; i < rows; i++) {
-      NSLog(@"row %i: ----------------------", i);
-      for (int j = 0; j < col; j++) {
-        NSLog(@"%s: %s", [[test.columns objectAtIndex: j] UTF8String],
-              [[[test.results objectAtIndex: i] objectAtIndex: j] UTF8String]);
-      }
-    }
-    [test dealloc];
-  }
-  return 0;
-}
-*/
