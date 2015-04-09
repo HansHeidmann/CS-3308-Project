88b64df702dda974edc8c4457f08305eb9d08091
Sean <sean.tranchetti@colorado.edu>
2015-04-09 16:04:47 -0600

    fixed inheritance issue, missing interface defn's, and macro def'n

--- BTSendRec.h
+++ BTSendRec.h
@@ -7,8 +7,19 @@
 
 #define BLE_UUID [CBUUID UUIDWithString: @"27EC8B94-9C1A-FE12-3669-780CD087A7A4"]
 
+#define RX_UUID
+
+#define TX_UUID
+
+#define RWT_BLE_SERVICE_CHANGED_STATUS_NOTIFICATION @"stuff goes here"
+
 //wrapper around CBPeripheral and acts as its delegate object
 //responsible for doing the data transmission to the connected peripheral
-@interface BTSendRec : NSObject
-- (instancetype) initWithPeripheral;
+@interface BTSendRec : NSObject <CBPeripheralDelegate>
+- (instancetype) initWithPeripheral:(CBPeripheral* peripheral);
+- (void) startDiscoveringServices;
+- (void) reset;
+- (void) peripheral: (CBPeripheral*) peripheral didDiscoverServices: (NSError*) error;
+- (void) peripheral: (CBPeripheral*) peripheral didDiscoverCharactisticsForService: (CBService*) service error: (NSError*) error;
+- (void) sendBTServicesNotificationWithIsBluetoothConnected: (BOOL) isBluetoothConnected;
 @end
