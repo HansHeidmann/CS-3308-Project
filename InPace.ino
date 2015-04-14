104c857ba2a586e8fe3369cd49c43d32ac9f3601
H-4-N-5 <hansheidmann@gmail.com>
2015-04-13 18:20:55 -0600

    Arduino code updated for RGB LED and the button for starting/stopping GPS data logging and the button for sending bluetooth data

--- InPace.ino
+++ InPace.ino
@@ -1,15 +1,37 @@
-/*
-    .  .    .   ___    ___     ____  _____ 
-    |  |\   |  |   \  |   |   /      |  
-    |  | \  |  |___|  |___|  |       |____
-    |  |  \ |  |      |   |  \       |
-    |  |   \|  |      |   |   \____  |____
-   
-    *******    by Hans Heidmann    ********
-   
-    ---------------------------- 
+/*  ______________________________________________
+   |     .  .    .   ___    ___     ____  _____  |
+   |     |  |\   |  |   \  |   |   /      |      |
+   |     |  | \  |  |___|  |___|  |       |____  |
+   |     |  |  \ |  |      |   |  \       |      |
+   |     |  |   \|  |      |   |   \____  |____  |
+   |                                             |
+   |*******       by Hans Heidmann        ********
+   |                                             |
+   |.......         Spring 2015           .......|
+   |                                             |
+   |_____________________________________________|
+    
+           ***   Parts List  ***
+           ---------------------
+                
+    1. Arduino Pro Mini
+    
+    2. Adafruit Ultimate GPS Breakout Board
+    
+    3. Four-Pin Addressable G-R-B LED
+    
+    4. Adafruit microSD Card 
+    
+    5. Sparkfun LiPo Powercell Charger/Booster (*Fried Booster,  Replacement #: 8750  should arrive by 4/17/2015)
+    
+    6. 2 Momentary Tactile Push Buttons (4 pin)
+    
+    7. Red Bear Labs Bluetooth Module
+            
+    
+    ---------------------------------------
      
-    SD Card Read/Write Pins
+    Adafruit microSD Card Read/Write Pins
  
     CS -> pin D-10
     MOSI -> pin D-11
@@ -18,21 +40,21 @@
 
     -----------------------------
     
-    GPS Module Pins
+    Adafruit Ultimate GPS_Module Module Pins
     
-    RX --> pin D-3
-    TX --> pin D-4
+    RX --> pin D-2
+    TX --> pin D-3
     
     -----------------------------
     
-    HM-10 BLE 4.0 Bluetooth Module Pins
+    Red Bear Labs BLE 4.0 Bluetooth Module Pins
     
     RX --> TX
     TX --> RX
     
     -----------------------------
     
-    RGB LED pins
+    Four-Pin Addressable G-R-B LED
     
          __   
         /  \
@@ -42,185 +64,279 @@
        | | | |
        | | | |
        | | | | 
-      1  | | 4
-         | 3
-         2
+       | | 3 4
+       | | 
+       1 2
   
-    (1) red pin
-    (2) common ground
-    (3) green pin
-    (4) blue pin
+    (1) logic input  -->  pin A0 (14)
+    (2) 4.5-6V VIN  -->  VCC
+    (3) ground  -->  GND
+    (4) locic output  -->  (Nothing, but could be used to chain more G-R-B LEDs together.)
+    
+    
+    -----------------------------
+    
+  
+    ////// systemState ////
+    
+    systemState = 0: initializing 
+                     - LED should be RED
+                     - attemping to get a GPS_Module Satellite fix
+    
+    
+    systemState = 1: Logging GPS_Module data
+                     - LED should be GREEN
+                     - open dataFile (GPS_Moduledata.txt) for writing to
+                     - read in GPS_Module data from software serial pins 2 and 3
+                     - write GPS_Module data to the microSD
+             
+    
+    systemState = 2: Sending Data Over Bluetooth
+                     - LED should be BLUE
+                     - open dataFile (GPS_Moduledata.txt) for reading from
+                     - iterate through all the lines of the text file
+                     - send each line of the text file through bluetooth as a string
+                     - *GPS_Moduledata.txt will be "rebuilt" string by string on the iOS receiving end
 
 */
 
 
-#include <SoftwareSerial.h>    // to use pins 3 and 4 for RX and TX with GPS module
-#include <TinyGPS.h>    // great backend library to support the GY-GPS6MV2
-#include <SPI.h>    // for writing/reading data to/from a micro SD card 
-#include <SD.h>    // for writing/reading data to/from a micro SD card 
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include "RTClib.h"
+
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
 
 
-File myFile;    // creates a "File" var that will be a pointer to the .txt file on the micro SD
-TinyGPS gps;    // creates a GPS object for the module
-SoftwareSerial ss(3, 2);    // initialize pins 3 and 2 to be used for transmitting and receiving GPS data
+///  G-R-B LED ///
+#define PIN            14  // (A0)
+#define NUMPIXELS      1  // just 1 G-R-B LED
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 
-int redPin = 9;
-int greenPin = 8;
-int bluePin = 7;
+/// Push Buttons ///
+const int GPS_Button_Pin = 9;
+int GPS_Button_State = 0;
 
-String RGB_LED_COLOR; //  = "off, "red", "green", "blue", "yellow", "purple", "teal"
+const int BLE_Button_Pin = 8;
+int BLE_Button_State = 0; 
+
+
+/// SD vars ///
+const int chipSelect = 10; // CS (chip select pin on microSD breakout board)
+File dataFile;  // creates a File called dataFile which will point to GPS_Moduledata.txt
+
+
+/// GPS_Module vars ///
+RTC_Millis RTC;  // create a Real-Time-Clocking time system
+TinyGPS GPS_Module;
+SoftwareSerial ss(3, 2); // pin 2 -> GPS_Module RX   and   pin 3 -> GPS_Module TX
+
+
+/// live comparison variables //
+/*
+int oldStartTime;
+int newerStartTime;  //keep initial time and location stamp
+*/
+
+//////////////////////////////
+/// **** SYSTEM STATE **** ///  See top of this file for description of what my different system states are used for. 
+//////////////////////////////
+                            //
+int systemState = 0;        //   
+                            //
+//////////////////////////////
 
 
 
-// define functions for GPS (full functions are below the main loop)
+/// function delcarations ///
 static void smartdelay(unsigned long ms);
-static void print_float(float val, float invalid, int len, int prec);
-static void print_int(unsigned long val, unsigned long invalid, int len);
-static void print_date(TinyGPS &gps);
-static void print_str(const char *str, int len);
+static void sendBluetooth();
 
 
-
-void setup()
-{
-  ss.begin(9600); // software serial for communication with GPS module
+////////  MAIN SETUP FUNCTION ////////
+void setup()  {
   
-  Serial.begin(9600); // open serial port for HM-10 BLE bluetooth module
-
-
+  pixels.begin();
   
-  pinMode(10, OUTPUT); //prepare pin 10 for CS pin on SD breakout
-
-  if (!SD.begin(10)) { //if SD isnt available, stop doign stuff
-    return;
+  pinMode(BLE_Button_Pin, INPUT);  // prepare pin 8 (bluetooth button) for detecting button press
+  pinMode(GPS_Button_Pin, INPUT);  // prepare pin 9 (gps mode button) for detecting button press
+  
+  Serial.begin(115200); // for debugging, *** remember to change to 9600 for bluetooth ***
+  Serial.print("Initializing SD card...");   // for debugging, *** remember to comment this out when using RX & TX for bluetooth! ***
+  
+  ss.begin(9600); // set the baud rate to 9600 for software serial pins 2 and 3 (GPS_Module)
+  
+  pinMode(chipSelect, OUTPUT); // set pin 10 as OUTPUT to ensure correct functionality of microSD sniffer
+  
+  RTC.adjust(DateTime(__DATE__, __TIME__));
+ 
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");  // for debugging, *** remember to comment this out when using RX & TX for bluetooth! ***
+    // don't do anything more:
+    while (1) ;
   }
-  //Serial.println("card access success"); 
-
-  myFile = SD.open("gpsdata.txt", FILE_WRITE);
-
-  if (myFile) {  // if the file opened okay, write to it:
-    Serial.print("Writing to gpsdata.txt...");
-    myFile.println("testing 1, 2, 3.");
-    myFile.close();// close the file:
-    Serial.println("done.");
-  } else {
-    Serial.println("error opening gpsdata.txt"); // if the file didn't open, print an error
+  Serial.println("card initialized.");  // for debugging, *** remember to comment this out when using RX & TX for bluetooth! ***
+ 
+  dataFile = SD.open("gpsdata.txt", FILE_WRITE);  //opens my gpsdata.text file for reading/writing
+  if (! dataFile) {
+    Serial.println("error opening gpsdata.txt");
+    // Wait forever since we cant write data
+    while (1) ;
   }
-
-  myFile = SD.open("gpsdata.txt"); // re-open the file for reading
-  if (myFile) {
-    Serial.println("gpsdata.txt:");
-    while (myFile.available()) {  // read from the file until there's nothing else in it
-      Serial.write(myFile.read());
-    }
-    myFile.close(); // close the file
-  } else {
-    Serial.println("error opening test.txt"); // if the file didn't open, print an error
-  }
+  
 }
 
 
+////////  MAIN LOOP ////////
+void loop()  {
+  
+  
+      DateTime now = RTC.now();  // set DateTime to current
+      bool newData = false;  // initializes newData as false, will become true once the GPS_Module module gets a satellite fix
+    
+      for (unsigned long start = millis(); millis() - start < 1000;)
+      {
+        while (ss.available()) // while data is coming from the GPS_Module module
+        {
+          char c = ss.read(); // read the GPS_Module data one character at a time
+          if (GPS_Module.encode(c)) // check to see if valid NMEA GPS_Module data is coming through yet (satellite fix yet?)
+            newData = true; // set to true if we have a satellite fix giving GPS_Module data
+        }
+      }
+      
+      
+      
+      if (newData)  {
+        
+          float flat, flon;
+          unsigned long age;
+          int year;
+          byte month, day, hour, minute, second, hundredths;
+          GPS_Module.f_get_position(&flat, &flon, &age);
+          GPS_Module.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+          char sz[32];
+          sprintf(sz, "%02d/%02d/%02d, %02d:%02d:%02d",
+          month, day, year, hour, minute, second);
+          
+    
+          Serial.print("");
+          Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+          Serial.print(", ");
+          Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+          Serial.print(", ");
+          Serial.println(sz);  
+          smartdelay(0);
+      
+          dataFile.print("");
+          dataFile.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+          dataFile.print(", ");
+          dataFile.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+          dataFile.print(", ");
+          dataFile.println(sz);  
+          smartdelay(0);
+      }
+      
+      
+      // if recording is not true:
+      // blah blah blah
+    
+      dataFile.flush();  // flush() ensures that the bytes were (or will be) written to the microSD like they should be
+      delay(100); // quick delay to give the processor some time to do the flush()
+      
+      
+      GPS_Button_State = digitalRead(GPS_Button_Pin);
+      if (GPS_Button_State == HIGH) { 
+          Serial.println("gps button pressed"); 
+          systemState = 1;   // **** Change system state to GPS_Module Mode (G-R-B LED will be Green)
+      }
+      
+      BLE_Button_State = digitalRead(BLE_Button_Pin);
+      if (BLE_Button_State == HIGH) { 
+          Serial.println("gps button pressed"); 
+          systemState = 2;   // **** Change system state to GPS_Module Mode (G-R-B LED will be Green)
+          sendBluetoothData();
+      }
+      
+      
+      ////////////////////////////////////
+      /// systemState G-R-B indication ///
+      ////////////////////////////////////
+      if (systemState == 0) {  
+        pixels.setPixelColor(0, pixels.Color(0,200,0)); // Moderately bright red color.
+        pixels.show(); // This sends the updated pixels color to the hardware.
+        delay(100); // this delay needs to happen in order to make the color change take effect 
+      } else if (systemState == 1) {
+        pixels.setPixelColor(0, pixels.Color(200,0,0)); // Moderately bright green color.
+        pixels.show(); // This sends the updated pixels color to the hardware.
+        delay(100); // this delay needs to happen in order to make the color change take effect
+      } else if (systemState == 2) {
+        pixels.setPixelColor(0, pixels.Color(0,0,200)); // Moderately bright blue color.
+        pixels.show(); // This sends the updated pixels color to the hardware.
+        delay(100); // this delay needs to happen in order to make the color change take effect
+      } else if (systemState == 3) {
+        pixels.setPixelColor(0, pixels.Color(0,200,200)); // Moderately bright blue color.
+        pixels.show(); // This sends the updated pixels color to the hardware.
+        delay(100); // this delay needs to happen in order to make the color change take effect
+      }
 
-void loop()
-{
-  float flat, flon;
-  unsigned long age, date, time, chars = 0;
-  unsigned short sentences = 0, failed = 0;
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
   
-  print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
-  print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
-  gps.f_get_position(&flat, &flon, &age);
-  print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
-  print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  print_date(gps);
-  print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-  print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-  print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
-  print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
   
-  gps.stats(&chars, &sentences, &failed);
-  print_int(chars, 0xFFFFFFFF, 6);
-  print_int(sentences, 0xFFFFFFFF, 10);
-  print_int(failed, 0xFFFFFFFF, 9);
-  Serial.println();
-  
-  smartdelay(1000);
 }
 
 
-
+////////  SMART DELAY FOR ERROR-FREE GPS_Module DATA ENCODING ////////
 static void smartdelay(unsigned long ms)
 {
   unsigned long start = millis();
   do 
   {
     while (ss.available())
-      gps.encode(ss.read());
+      GPS_Module.encode(ss.read());
   } while (millis() - start < ms);
 }
 
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
+
+static void sendBluetoothData() {
+//
+//  when tactile push button is pressed for bluetooth:
+//
+// - pair with iphone 
+// - grab GPS_Module data from microSD
+// - send through arduino TX to BLE module RX
+// - send through BLE module TX to iphone
+//
+//
+//
+/*
+  if (bluetooth button was pressed) {
+    
+    myFile = SD.open("test.txt");
+    
+    if (myFile) {
+      Serial.println("test.txt:");
+    
+      while (myFile.available()) {
+        
+        Serial.write(myFile.read()); // read from the file until there's nothing else in it
+      }
+    
+      myFile.close(); // close the file:
+      
+  } else {
+    Serial.println("error opening test.txt");  // if the file didn't open, print an error
   }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
+    
+  
   }
-  smartdelay(0);
+*/
+
+
 }
 
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartdelay(0);
-}
-
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  smartdelay(0);
-}
-
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartdelay(0);
-}
