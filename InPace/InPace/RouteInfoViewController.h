db6acde1db0c45a0383165dd28e160b520f9b61f
Madison Rockwell <madison.rockwell@colorado.edu>
2015-04-26 17:04:49 -0600

    fixed header search path

--- InPace/InPace/RouteInfoViewController.h
+++ InPace/InPace/RouteInfoViewController.h
@@ -21,7 +21,6 @@
 @property (strong, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 - (IBAction)segmentValueChanged:(id)sender;
 
-
 @property (nonatomic, retain) IBOutlet MKMapView *mapView;/**<Map View*/
 
 @property (nonatomic) long long int routeID;/**<holds ID of current route*/
